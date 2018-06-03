afb1d78bdf5c608d4c6556836670194b213bbd09
syntel <chrsitw@g.clemson.edu>
2018-06-02 21:29:18 -0400

    Touch up some CSS

--- src/App.js
+++ src/App.js
@@ -177,6 +177,7 @@ class App extends Component {
 	render(){
 		return(
 			<div id="main">
+        <h1>The Game of Life</h1>
 				<Board 
 					color={this.state.color}
 					neighCount={this.state.neighCount}
